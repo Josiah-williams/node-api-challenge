d40416c51b5b7d11b5fb930cd0901acb5169e633
josiah <josiahdamiwilliams@gmail.com>
2020-03-13 17:35:49 +0100

    added projectRouter

---
+++ projectsRouter.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const projectsHelpers = require("./data/helpers/projectModel");
+const projects = express.Router();
+const middleware = require("./middleware") 
+
+
+projects.get("/", (req, res) => {
+    projectsHelpers
+      .get()
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(err => {
+        res.status(500);
+      });
+  });
+
+  projects.get("/:id", (req, res) => {
+    const {id} = req.params;
+  projectsHelpers
+    .get(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+projects.get("/:id/actions", (req, res) => {
+    const {id} = req.params;
+  projectsHelpers
+    .getProjectActions(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+projects.post("/", (req, res) => {
+    const project = req.body;
+  projectsHelpers
+    .insert(project)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+projects.put("/:id", (req, res) => {
+    const {id} = req.params;
+    const change = req.body;
+  projectsHelpers
+    .update(id, change)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+projects.delete("/:id", (req, res) => {
+    const {id} = req.params;
+  projectsHelpers
+    .remove(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+module.exports = projects;
