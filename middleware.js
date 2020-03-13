e02ff62be3eda0c0facfeb471cd239593fe018c5
josiah <josiahdamiwilliams@gmail.com>
2020-03-13 17:21:05 +0100

    add middleware

---
+++ middleware.js
@@ -0,0 +1,21 @@
+module.exports = {
+    ValidateActionsID,
+    ValidateProjectsID
+}
+
+function ValidateActionsID(req, res, next) {
+    if (isNaN(req.params.id)) {
+        res.status(400).json("Please provide a correct ID")
+    } else {
+        req.actions = {...req.params}
+        next()
+    }
+}
+
+function ValidateProjectsID(req, res, next) {
+    if (isNaN(req.params.id)) {
+        res.status(400).json("Please provide a correct ID")
+    } else {
+        req.projects = {...req.params}
+        next()
+    }
